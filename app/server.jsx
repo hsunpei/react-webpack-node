261ae9d6a024cee7e27c4462ce2c4a20cb0dc8c5
Ken Ding <aenkil@gmail.com>
2015-11-23 21:11:19 +1100

    formatting and braces into proper blocks.

--- app/server.jsx
+++ app/server.jsx
@@ -19,18 +19,19 @@ const renderToMarkup = (store, req, res) => {
   let location = new createLocation(req.url);
 
   match({ routes, location }, (error, redirectLocation, renderProps) => {
-    if (redirectLocation)
-      res.redirect(301, redirectLocation.pathname + redirectLocation.search)
-    else if (error)
-      res.send(500, error.message)
-    else if (renderProps == null)
-      res.send(404, 'Not found')
-    else
-      content = ReactDOMServer.renderToString(
+    if (redirectLocation) { 
+      res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+    } else if (error) {
+      res.status(500).send(error.message);
+    } else if (renderProps == null) {
+      res.status(404).send('Not found');
+    } else {
+      content = renderToString(
         <Provider store={store}>
           <RoutingContext {...renderProps} />
         </Provider> );
       markup = Iso.render(content, store.getState());
+    }
   });
 
   return markup;
